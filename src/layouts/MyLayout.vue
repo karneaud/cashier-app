9ddea30309909808fe3fba4ff8d03d80b1d1985a
karneaud <kendall.arneaud@gmail.com>
2019-03-31 16:18:51 -0400

    init

---
+++ src/layouts/MyLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header reveal class="bg-primary text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+         	Cashier Application
+          <div slot="subtitle">v0.1</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-page class="flex flex-center">
+    <h1>Test</h1>
+  </q-page>
+</template>
+<style>
+</style>
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
