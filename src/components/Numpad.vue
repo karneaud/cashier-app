610b0fd3a7ea44aea824b9687dc994fc3851d785
karneaud <kendall.arneaud@gmail.com>
2019-03-31 20:53:20 -0400

    construct number pad from for loop with onclick

--- src/components/Numpad.vue
+++ src/components/Numpad.vue
@@ -1,13 +1,28 @@
 <template>
-  <div>My component</div>
+  <div class="numpad">
+    <div class="row">
+      <div class="col-4" v-for="n in this.keypads">
+         <q-btn flat color="primary" size="xl"  :label="n" @click="press(n)"/>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+
 export default {
-  // name: 'ComponentName',
-  data () {
-    return {}
-  }
+  name: 'Numpad',
+  props: {
+    keypads :  {
+      type: Array,
+      default: () => [1,2,3,4,5,6,7,8,9,0,'*','%','.','-','+']
+    }
+  },
+  methods: {
+    press(val) {
+      this.$emit('pressed', val)
+    }
+   }
 }
 </script>
 
