5ef4ba8041e41f90c47dded797efebf021221f76
karneaud <kendall.arneaud@gmail.com>
2019-04-04 14:33:16 -0400

    get all input and then send. add colour for send button

--- src/components/Numpad.vue
+++ src/components/Numpad.vue
@@ -1,26 +1,33 @@
 <template>
   <div class="numpad">
     <div class="row">
-      <div class="col-4" v-for="n in this.keypads">
-         <q-btn flat color="primary" size="xl"  :label="n" @click="press(n)"/>
-      </div>
+      <q-btn v-for="n in this.keypads" class="col-4" :flat="n !== 'add'" :color="n === 'add'? 'red' : 'primary'" size="xl"  :label="n" @click="press(n)"/>
     </div>
   </div>
 </template>
-
 <script>
-
 export default {
   name: 'Numpad',
   props: {
     keypads :  {
       type: Array,
-      default: () => [1,2,3,4,5,6,7,8,9,0,'*','%','.','-','+']
+      default: () => [1,2,3,4,5,6,7,8,9,0,'*','%',"add"]
+    }
+  },
+  data() {
+    return {
+      numbers: "",
+      operator: "*"
     }
   },
   methods: {
-    press(val) {
-      this.$emit('pressed', val)
+    press(key) {
+      if(isNaN(key))this.operator = key
+      else this.numbers.concact(key)
+    },
+    send() {
+      let value = new Number(this.numbers).valueOf()
+      this.$emit('pressed', value, operator)
     }
    }
 }
