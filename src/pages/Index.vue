5e7067783f578385468cfb11fd17a438aa3cb62e
karneaud <kendall.arneaud@gmail.com>
2019-04-04 11:55:10 -0400

    move list over to component

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,27 +1,24 @@
 <template>
   <q-page class="flex flex-center">
-    <div class="row">
-      <div class="col-6">
-        <div class="container scroll">
-          <q-list bordered separator>
-            <q-item clickable v-ripple>
-              <q-item-section>
-              <q-item-label overline>OVERLINE</q-item-label>
-              <q-item-label>Item with caption</q-item-label>
-              </q-item-section>
-            </q-item>
-          </q-list>
+    <div style="width: 100%; height: calc(100vh - 112px); overflow:hidden">
+      <div class="row" style="height: inherit">
+        <div class="col scroll" style="height: 100%">
+          <div class="container">
+            <menuitem-list @selectedItem="itemSelected"></menuitem-list>
+          </div>
         </div>
-      </div>
-      <div class="col-6">
-        <numpad @pressed="keypadPressed"></numpad>
+        <div class="col">
+          <numpad @pressed="keypadPressed"></numpad>
+        </div>
       </div>
     </div>
   </q-page>
+
 </template>
 <style>
 </style>
 <script>
+
 export default {
   name: 'PageIndex',
   data() {
@@ -29,9 +26,13 @@ export default {
       items: []
     }
   },
+
   methods: {
     keypadPressed(val) {
       console.log('key pressed', val);
+    },
+    itemSelected(item) {
+      console.log('item selected is', item )
     }
   }
 }
