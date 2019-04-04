951d82256f1621692b7e793798c277bb7efba004
karneaud <kendall.arneaud@gmail.com>
2019-04-04 11:54:59 -0400

    add footer

--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -11,6 +11,16 @@
     <q-page-container>
       <router-view />
     </q-page-container>
+    <q-footer class="bg-grey-8 text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar-framework.org/logo/svg/quasar-logo.svg">
+          </q-avatar>
+          Title
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
   </q-layout>
 </template>
 <script>
