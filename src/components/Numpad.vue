c826a7649663ce0c718e373425c9a7fbb00f7df9
karneaud <kendall.arneaud@gmail.com>
2019-04-05 09:59:36 -0400

    Add clear and add button. concat numbers

--- src/components/Numpad.vue
+++ src/components/Numpad.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="numpad">
     <div class="row">
-      <q-btn v-for="n in this.keypads" class="col-4" :flat="n !== 'add'" :color="n === 'add'? 'red' : 'primary'" size="xl"  :label="n" @click="press(n)"/>
+      <q-btn v-for="n, index in this.keypads" :key="index" class="col-4" :flat="n !== 'add'" :color="isKey(n)" size="xl"  :label="n" @click="press(n)"/>
     </div>
   </div>
 </template>
@@ -11,7 +11,7 @@ export default {
   props: {
     keypads :  {
       type: Array,
-      default: () => [1,2,3,4,5,6,7,8,9,0,'*','%',"add"]
+      default: () => [1,2,3,4,5,6,7,8,9,0,'*','%',"add","clear"]
     }
   },
   data() {
@@ -20,14 +20,41 @@ export default {
       operator: "*"
     }
   },
+  computed: {
+    amount() {
+      return this.numbers == ""? 1 : new Number(this.numbers).valueOf()
+
+    }
+  },
   methods: {
+    isKey(key) {
+      var cl = "primary"
+      switch (key) {
+        case "add":
+          cl = "secondary"
+          break;
+        case "clear":
+          cl = "red"
+          break;
+        default:
+          break;
+      }
+
+      return cl
+    },
     press(key) {
-      if(isNaN(key))this.operator = key
-      else this.numbers.concact(key)
+    if(key == "add") this.send()
+      else if(key == "clear") this.clear()
+      else if(isNaN(key))this.operator = key
+      else this.numbers = this.numbers.concat(key)
     },
     send() {
-      let value = new Number(this.numbers).valueOf()
-      this.$emit('pressed', value, operator)
+      this.$emit('pressed', this.amount, this.operator)
+      this.clear()
+    },
+    clear() {
+      this.operator = "*"
+      this.numbers = ""
     }
    }
 }
