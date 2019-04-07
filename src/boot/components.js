2e9eada64db885ae7bdaffe9cf5dd457920674b7
karneaud <kendall.arneaud@gmail.com>
2019-04-06 20:24:25 -0400

    add component

--- src/boot/components.js
+++ src/boot/components.js
@@ -1,8 +1,10 @@
 // import something here
 import Numpad from '../components/Numpad.vue'
 import MenuItemList from '../components/MenuItemList.vue'
+import InputDisplay from '../components/InputDisplay.vue'
 // leave the export, even if you don't use it
 export default async ({ app, router, Vue }) => {
   Vue.component('numpad', Numpad)
   Vue.component('menuitem-list', MenuItemList)
+  Vue.component('input-display', InputDisplay)
 }
