3d7f55e854869a4f541354d5eaf4686e7f164f2c
karneaud <kendall.arneaud@gmail.com>
2019-04-11 13:05:07 -0400

    integrate shopping list component

--- src/boot/components.js
+++ src/boot/components.js
@@ -2,9 +2,11 @@
 import Numpad from '../components/Numpad.vue'
 import MenuItemList from '../components/MenuItemList.vue'
 import InputDisplay from '../components/InputDisplay.vue'
+import ShoppingList from '../components/ShoppingList.vue'
 // leave the export, even if you don't use it
 export default async ({ app, router, Vue }) => {
   Vue.component('numpad', Numpad)
   Vue.component('menuitem-list', MenuItemList)
   Vue.component('input-display', InputDisplay)
+  Vue.component('shopping-list', ShoppingList)
 }
