5358b809460e448f72d9f20ecdbf4cb501681ee0
karneaud <kendall.arneaud@gmail.com>
2019-04-13 19:26:24 -0400

    refactor list style. add item badge

--- src/components/ShoppingList.vue
+++ src/components/ShoppingList.vue
@@ -3,21 +3,26 @@
  enter-active-class="animated slideInUp"
  leave-active-class="animated slideOutDown">
     <div v-show="showList" id="shopping-basket" style="width: 35%;" class="fixed-bottom bg-white">
-      <div class="q-pa-sm">
-        <h6 class="q-my-sm">Cash Items:</h6>
-        <q-list v-if="items" id="shoppin-list" separator class="scroll">
-          <q-item v-ripple v-for="(item, index) in items" :key="(item.referenceid) + 1">
+      <div class="q-py-sm">
+        <h6 class="q-my-sm text-center">
+          Cash Items:
+        </h6>
+        <q-separator spaced inset />
+        <q-list v-show="items.length > 0" id="shoppin-list" separator class="scroll">
+          <q-item v-ripple v-for="(item, index) in items" :key="(item.referenceid) + 1" class="q-pr-none">
+            <q-item-section avatar>
+                <q-icon @click="removeItem(index)" name="close"/>
+              </q-item-section>
             <q-item-section>
-            <q-item-label>{{ item.productitem }} x {{ item.qty }}</q-item-label>
+              <q-item-label>{{ item.productitem }}</q-item-label>
             </q-item-section>
             <q-item-section side>
-              <q-icon @click="removeItem(index)" name="remove"/>
+              <q-badge color="primary"> {{ item.qty }}</q-badge>
             </q-item-section>
              <q-separator spaced inset />
           </q-item>
-
         </q-list>
-        <h6 v-show="!items">No Items!</h6>
+        <h6 class="q-my-sm text-center" v-show="items.length == 0">No Items!</h6>
       </div>
     </div>
   </transition>
