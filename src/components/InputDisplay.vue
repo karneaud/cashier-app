0ec5d0dd97b6163e862267d20181b0c95f83f5f3
karneaud <kendall.arneaud@gmail.com>
2019-04-13 13:14:40 -0400

    refactor layout of total values elements

--- src/components/InputDisplay.vue
+++ src/components/InputDisplay.vue
@@ -1,27 +1,16 @@
 <template lang="html">
-  <div id="input-display">
-    <div class="row text-red border-bottom q-my-md">
-        <div class="col">
-          <span class="subtitle">TOTAL: </span>
-        </div>
-        <div class="col text-right">
-          <span class="text-h3">${{ totalAmount | formatNumber }}</span>
-        </div>
+  <div id="input-display" class="q-mt-sm">
+    <div class="row">
+      <div class="col text-red">
+        <span class="subtitle">TOTAL: </span>
+        <p class="text-h3">${{ totalAmount | formatNumber }}</p>
       </div>
-      <div class="row q-mb-md">
-        <div class="col">
-            <q-input @blur="() => this.paid > 0? this.paid : 0" @focus="() => this.paid = this.paid > 0? this.paid : ''" label-stacked label="ENTER AMOUNT PAID:" prefix="$" v-model="paid"/>
-          </div>
-    </div>
-    <div class="row text-green">
-       <div class="col">
-          <span class="subtitle">BALANCE: </span>
-        </div>
-        <div class="col text-right">
-          <span class="text-h4">${{ balance | formatNumber }}</span>
-        </div>
+      <div class="col text-right text-green">
+        <span class="subtitle">BALANCE: </span>
+        <p class="text-h5">${{ balance | formatNumber  }}</p>
       </div>
     </div>
+    <q-input @focus="() => this.paid = ''" label-stacked label="ENTER AMOUNT PAID:" prefix="$" v-model="paid"/>
   </div>
 </template>
 <script>
@@ -49,14 +38,5 @@ export default {
 }
 </script>
 <style lang="scss">
-#input-display {
-  .q-field--float .q-field__label {
-    transform: translate3d(0, -80%, 0) scale3d(0.75, 0.75, 0.75);
-  }
 
-  .q-field--labeled .q-field__native {
-    font-size: 2.4rem;
-    padding-bottom: 10px;
-  }
-}
 </style>
