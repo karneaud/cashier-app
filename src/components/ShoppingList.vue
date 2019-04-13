96505047fde2f7c93bd8b669b6fbb077353a5d66
karneaud <kendall.arneaud@gmail.com>
2019-04-13 13:15:29 -0400

    remove border from list

--- src/components/ShoppingList.vue
+++ src/components/ShoppingList.vue
@@ -5,7 +5,7 @@
     <div v-show="showList" id="shopping-basket" style="width: 35%;" class="fixed-bottom bg-white">
       <div class="q-pa-sm">
         <h6 class="q-my-sm">Cash Items:</h6>
-        <q-list v-if="items" id="shoppin-list" bordered separator class="scroll">
+        <q-list v-if="items" id="shoppin-list" separator class="scroll">
           <q-item v-ripple v-for="(item, index) in items" :key="(item.referenceid) + 1">
             <q-item-section>
             <q-item-label>{{ item.productitem }} x {{ item.qty }}</q-item-label>
@@ -13,7 +13,9 @@
             <q-item-section side>
               <q-icon @click="removeItem(index)" name="remove"/>
             </q-item-section>
+             <q-separator spaced inset />
           </q-item>
+
         </q-list>
         <h6 v-show="!items">No Items!</h6>
       </div>
