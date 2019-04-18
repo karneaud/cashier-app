b2fed87c22e6a76581ba05fa2f14d1d1e9805834
karneaud <kendall.arneaud@gmail.com>
2019-04-17 22:27:13 -0400

    add start url

--- quasar.conf.js
+++ quasar.conf.js
@@ -103,6 +103,7 @@ module.exports = function (ctx) {
         //   }
         // ]
       },
+      start_url: '/',
       manifest: {
         name: 'Cashier App',
         short_name: 'Mobile Cashier Application',
