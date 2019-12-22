dceb681e75ce5c43239c8217efc2039580b58602
karneaud <kendall.arneaud@gmail.com>
2019-12-22 15:25:27 -0400

    map actions and setup validation

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -9,16 +9,13 @@
               <q-card square bordered class="q-pa-lg shadow-1">
                 <q-card-section>
                   <q-form class="q-gutter-md">
-                    <q-input filled v-model="email" type="email" suffix="@gmail.com" label="Enter Gmail">
-                    <template v-slot:before>
-                      <q-icon name="mail" />
-                    </template>
+                    <q-input filled v-model="email" type="text" suffix="@gmail.com" label="Enter Gmail">
                   </q-input>
                     <q-input square filled clearable v-model="sheetId" type="text" label="Sheet ID#" />
                   </q-form>
                 </q-card-section>
                 <q-card-actions class="q-px-md">
-                  <q-btn unelevated color="light-green-7" size="lg" class="full-width" label="Login" />
+                  <q-btn ref="loginBtn" :disable="disabled" unelevated color="light-green-7" size="lg" class="full-width" @click="setUserValues" label="Login" />
                 </q-card-actions>
                 <q-card-section class="text-center q-pa-none">
                   <p class="text-grey-6">Not reigistered? Created an Account</p>
@@ -31,26 +28,40 @@
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapActions } from 'vuex'
 
 export default {
   name: 'Login',
   data () {
     return {
-
+      username: null,
+      sheetId: null
     }
   },
   computed: {
-    ...mapState({
-      email: {
-        set(email) { state.user.email = email},
-        get: () => state.user.email
+    email: {
+      set(email) {
+        this.username = `${ (/@/g).test(email)? email.substr(0, email.indexOf('@')) : email }@gmail.com`
       },
-      sheetId: {
-        set(id) { state.user.id =  id},
-        get: () => state.user.id
+      get(){
+        let uname = (this.username == null? '' : this.username)
+
+        return uname.substr(0, uname.indexOf('@'))
       }
-    })
+    },
+    disabled() {
+      return (this.username == null && this.sheetId == null) || (this.sheetId == null)
+    }
+  },
+  methods: {
+    setUserValues() {
+      if(this.username && this.sheetId) {
+        this.setUserEmail(this.username)
+        this.setSheetId(this.sheetId)
+      }
+
+    },
+    ...mapActions('user', ['setUserEmail', 'setSheetId'])
   }
 }
 </script>
