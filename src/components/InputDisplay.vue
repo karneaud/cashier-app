9eec47db101caaf5c198d9c7cd78c38a9b79c34e
karneaud <kendall.arneaud@gmail.com>
2019-12-25 17:31:31 -0400

    refactor amount layouts

--- src/components/InputDisplay.vue
+++ src/components/InputDisplay.vue
@@ -1,16 +1,24 @@
 <template lang="html">
   <div id="input-display" class="q-mt-sm">
     <div class="row">
-      <div class="col text-red">
-        <span class="subtitle">TOTAL: </span>
-        <p class="text-h3">${{ totalAmount | formatNumber }}</p>
+      <div class="col col-4">
+        <div class="container full">
+          <span class="subtitle">TOTAL:&nbsp;</span>
+          <span class="text-h3">${{ totalAmount | formatNumber }}</span>
+        </div>
       </div>
-      <div class="col text-right text-green">
-        <span class="subtitle">BALANCE: </span>
-        <p class="text-h5">${{ balance | formatNumber  }}</p>
+      <div class="col col-3">
+        <div class="container full">
+          <q-input :input-style="{ color: '#fff' }" filled item-aligned dense hide-bottom-space @focus="() => this.paid = ''" prefix="PAID: $" v-model="paid"/>
+        </div>
+      </div>
+      <div class="col col-5">
+        <div class="container full" style="height:100%;">
+          <span class="subtitle">BALANCE:&nbsp;</span>
+          <span class="text-h5">${{ balance | formatNumber  }}</span>
+        </div>
       </div>
     </div>
-    <!-- <q-input @focus="() => this.paid = ''" label-stacked label="ENTER AMOUNT PAID:" prefix="$" v-model="paid"/> -->
   </div>
 </template>
 <script>
@@ -38,5 +46,19 @@ export default {
 }
 </script>
 <style lang="scss">
+  footer {
+    .row {
+      .col {
+        .container {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+        }
+      }
+    }
 
+    .q-field__native, .q-field__prefix, .q-field__suffix {
+      color: white;
+    }
+  }
 </style>
