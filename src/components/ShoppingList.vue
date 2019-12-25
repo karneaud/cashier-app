750d32705bdbd4f2571861e73aceedb60d8f92f5
karneaud <kendall.arneaud@gmail.com>
2019-12-25 18:26:19 -0400

    refactor to include price, sum total and #items

--- src/components/ShoppingList.vue
+++ src/components/ShoppingList.vue
@@ -1,26 +1,30 @@
 <template lang="html">
   <div id="shopping-basket" class="bg-white">
-      <div class="q-py-sm">
+      <div class="q-mb-lg">
+        <header>
         <h6 class="q-my-sm text-center">
-          Cash Items:
+          Cash Items: <span class="q-badge">{{ total }}</span>
         </h6>
+      </header>
         <q-separator spaced inset />
-        <q-list v-show="items.length > 0" id="shoppin-list" separator>
-          <q-item v-ripple v-for="(item, index) in items" :key="(item.referenceid) + index" class="q-pr-none">
+        <q-list v-show="total > 0" id="shoppin-list" separator>
+          <q-item v-ripple v-for="(item, index) in items" :key="(item.referenceid) + index" class="q-pr-none q-mb-sm">
             <q-item-section avatar>
                 <q-icon @click="removeItem(index)" name="close"/>
               </q-item-section>
             <q-item-section>
               <q-item-label>{{ item.productitem }}</q-item-label>
+              <q-item-label caption>${{ ((parseFloat(item.cost) + item.multiplier) * item.qty) | formatNumber }}</q-item-label>
             </q-item-section>
             <q-item-section side>
-              <q-badge color="primary"> {{ item.qty }}</q-badge>
+              <q-badge color="primary">{{ item.qty }}</q-badge>
             </q-item-section>
              <q-separator spaced inset />
           </q-item>
         </q-list>
         <h6 class="q-my-sm text-center" v-show="items.length == 0">No Items!</h6>
       </div>
+      <p class="fixed-bottom q-mb-none q-pa-sm text-center bg-blue-grey-1">Total:&nbsp;<small>${{ totalAmount | formatNumber }}</small></p>
     </div>
 </template>
 <script>
