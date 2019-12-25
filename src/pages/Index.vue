8a2b31cf4b257cc13f88619bf2e99fae9b16c95e
karneaud <kendall.arneaud@gmail.com>
2019-12-25 17:32:14 -0400

    move input to footer

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -6,14 +6,7 @@
               <menuitem-list></menuitem-list>
             </div>
             <div class="col-5">
-              <div class="fit column q-pa-sm">
-                <div class="col-8">
-                  <numpad></numpad>
-                </div>
-                <div class="col-2">
-                  <input-display></input-display>
-                </div>
-              </div>
+              <numpad></numpad>
             </div>
           </div>
         </div>
