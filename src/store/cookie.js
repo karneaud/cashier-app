609aa1c307fd254b7cdbd3b2226174f14a830428
karneaud <kendall.arneaud@gmail.com>
2019-12-31 13:55:15 -0400

    if no transactions clear cookie

--- src/store/cookie.js
+++ src/store/cookie.js
@@ -3,10 +3,13 @@ import VuexPersistence from 'vuex-persist'
 
 const VuexCookie = new VuexPersistence({
   restoreState: (key, storage) => Cookies.getJSON(key),
-  saveState: (key, state, storage) =>
-    Cookies.set(key, state, {
+  saveState: (key, state, storage) => {
+    if(state.transactions.transactions.length == 0) {
+      Cookies.remove(key)
+    } else Cookies.set(key, state, {
       expires: 1
-    }),
+    })
+  },
   modules: ['transactions']
 })
 
