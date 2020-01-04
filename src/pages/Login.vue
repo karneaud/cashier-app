1e29d49045b7881871671af56cfdd2152e261a5d
karneaud <kendall.arneaud@gmail.com>
2020-01-04 17:29:47 -0400

    use custom form component

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,98 +1,36 @@
 <template>
   <q-layout>
-  <q-page-container>
-    <q-page>
-      <div class="q-pa-md no-scroll window-height">
-        <div class="shadow-1 container rounded-borders fixed-center q-pa-sm width-75">
-          <div class="row">
-            <div class="col-4 q-mr-none">
-              <div class="container full-height center-content" style="flex-direction:column">
-                <q-avatar size="128px" class="q-ma-md">
-                  <img src="https://place-hold.it/300x500" />
-                </q-avatar>
-                <small><a href="">Need Help?</a></small>
+    <q-page-container>
+      <q-page>
+        <div class="q-pa-md no-scroll window-height">
+          <div class="shadow-1 container rounded-borders fixed-center q-pa-sm width-75">
+            <div class="row">
+              <div class="col-4 q-mr-none">
+                <div class="container full-height center-content" style="flex-direction:column">
+                  <q-avatar size="128px" class="q-ma-md">
+                    <img src="https://place-hold.it/300x500" />
+                  </q-avatar>
+                  <small><a href="">Need Help?</a></small>
+                </div>
               </div>
-            </div>
-            <div class="col">
-              <div class="container">
-                <q-form class="q-pa-md">
-                  <div class="row q-gutter-md items-start">
-                    <q-input filled hint="without @gmail" class="full-width" v-model="address" type="text" suffix="@gmail.com" label="Enter Gmail"/>
-                    <q-input square hint="URL e.g. https://docs.google.com/spreadsheets/d/.../edit" class="full-width" filled clearable v-model="url" type="text" label="Sheet ID#/ URL" />
-                    <q-btn ref="loginBtn" :loading="loaded" :disable="disabled" unelevated color="light-green-7" size="lg" class="full-width" @click="setUserValues" label="Login" />
-                  </div>
-                  <div class="row">
-                    <a href=""><small>See how you create your spreadsheet here</small></a>
-                  </div>
-                </q-form>
+              <div class="col">
+                <div class="container">
+                  <k-form @saved="(() => this.$router.push({ path: '/' }))" buttonText="Login"/>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </q-page>
-  </q-page-container>
-</q-layout>
-
+      </q-page>
+    </q-page-container>
+  </q-layout>
 </template>
 
 <script>
-import drive from 'drive-db'
-import { mapActions } from 'vuex'
-
-let r = /^https:\/\/docs\.google\.com\/spreadsheets\/d\/(.+)\//
-
-export default {
-  name: 'Login',
-  data () {
-    return {
-      username: null,
-      sheet: null,
-      loading: false
-    }
-  },
-  computed: {
-    address: {
-      set(email) {
-        this.username = `${ (/@/g).test(email)? email.substr(0, email.indexOf('@')) : email }@gmail.com`
-      },
-      get(){
-        let uname = (this.username == null? '' : this.username)
-
-        return uname.substr(0, uname.indexOf('@'))
-      }
-    },
-    url: {
-      set(s) {
-        this.sheet = r.test(s)? s.match(r)[1] : s
-      },
-      get() {
-        return this.sheet
-      }
-    },
-    loaded () {
-      return this.loading
-    },
-    disabled() {
-      return (this.username == null && this.sheet == null) || (this.sheet == null)
-    }
-  },
-  methods: {
-    setUserValues() {
-      if(this.username && this.sheet) {
-        this.setUserEmail(this.username)
-        this.setSheetId(this.sheet)
-        this.loading = true
-        drive({sheet: this.sheet, cache: 0}).catch((e) => console.log(e)).then(db => {
-          return this.$router.push({ path: '/' })
-        }).finally(() => this.loading = false)
-      }
-    },
-    ...mapActions('user', ['setUserEmail', 'setSheetId'])
+  export default {
+    name: 'Login'
   }
-}
 </script>
-
 <style>
 .width-75 {
   max-width: 75%;
