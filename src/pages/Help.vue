f62a96be4fb85ea8400c48857bae14c151aa07cf
karneaud <kendall.arneaud@gmail.com>
2020-01-05 15:58:42 -0400

    update help

--- src/pages/Help.vue
+++ src/pages/Help.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="q-pa-md">
-    <h2 class="text-center">Help :: Using the Cashier Application</h2>
+    <q-toolbar>
+      <q-btn dense flat icon="arrow_back" @click="(() => this.$router.push({ path: '/' }))" />
+      <q-toolbar-title class="text-center text-h4">
+        Help :: Using the Cashier Application
+      </q-toolbar-title>
+    </q-toolbar>
     <section class="container">
       <header class="text-center">
         <h5 class="q-my-md">Creating a Spread Sheet</h5>
@@ -21,11 +26,17 @@
           <ul>
             <li>Searchable list of product items with their cost.</li>
             <li>Cash items list display on selected items.</li>
-            <li>The ability to calculate discount on cost per selected item.</li>
+            <li>The ability to calculate discount on cost item total.</li>
             <li>A History of all cashed/ paid transactions for one day.</li>
           </ul>
-          <p>To cash/ calculate customers item(s), search/ scroll for the item in the list and simply select the line item. Once selected the item will be added to the cash item list display and the total cost immediately calculated. Selecting and item will default the quantity to cash for at 1. To increase the quantity, just select the quantity from the number pad and tap the <strong>"Add"</strong> button. This will update the quantity and re-calculate the total amount to cash for.</p>
-          <p>To add a discount, select the amount from the number pad and also the <strong>%</strong> symbol <strong>BEFORE</strong> pressing the <strong>Add</strong> button. This will </p>
+          <h6 class="q-my-sm">Adding an Item to Cash</h6>
+          <p>To cash/ calculate customers item(s), search/ scroll for the item in the list and simply select the line item. Once selected the item will be added to the cash item list display and the total cost immediately calculated. Selecting and item will default the quantity to cash for at 1. To change the quantity, just punch in the quantity from the number pad and tap the <strong>"Add"</strong> button. This will update the quantity and re-calculate the total amount to cash for.</p>
+          <h6 class="q-my-sm">Adding a Discount to an Item</h6>
+          <p>To add a discount, select the amount percentage from the number pad and also the <strong>%</strong> symbol <strong>BEFORE</strong> pressing the <strong>Add</strong> button. This will calculate a discount on the unit cost of the item</p>
+          <h6 class="q-my-sm">Viewing Items to Cash</h6>
+          <p>To review the items to cash tap the <strong><i aria-hidden="true" class="material-icons q-icon">shopping_basket</i></strong> icon at the top right of the interface. A list of the items would appear on the left hand side of the interface. You can remove an item by either clicking the corresponding "X" icon. You can also use the <strong>"Clear"</strong> button on the number pad which will remove the last item added to the list. Clicking the <strong>Reset</strong> would remove <strong>ALL</strong> items from the list.</p>
+          <h6 class="q-my-sm">Viewing Past Transactions</h6>
+          <p>When you recieve payment, entering the amount paid into the space provided to calculate the balance if any. Then click the <strong>Reset</strong> button to clear the list and save the transaction. If you <strong>DO NOT</strong> enter an amount paid the transaction would <strong>NOT</strong> be saved on reset. To view the saved transactions click on the <strong><i aria-hidden="true" class="material-icons q-icon">receipt</i></strong> icon at the top right of the application interface Transactions are saved for <strong>One (1) day</strong> before being cleared from history. You can download your transaction data by clicking the <strong><i aria-hidden="true" class="material-icons q-icon">cloud_download</i></strong> icon. This will export the data to a Comma Separated Values(CSV) file. You can maunally clear all saved transactions by clicking the <strong><i aria-hidden="true" class="material-icons q-icon">delete</i></strong> icon</p>
       </article>
     </section>
   </div>
@@ -33,6 +44,6 @@
 
 <script>
 export default {
-  name: 'Error404'
+  name: 'Help'
 }
 </script>
