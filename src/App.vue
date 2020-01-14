5700339d74ce1681aceb55fe553f4ab8f9846e51
karneaud <kendall.arneaud@gmail.com>
2020-01-13 20:59:03 -0400

    refactor add to homescreen dialog

--- src/App.vue
+++ src/App.vue
@@ -3,58 +3,19 @@
     <router-view />
     <div class="not-standalone fullscreen fixed-center full-height bg-white fit full-width" v-show="!standalone">
       <section>
-        <header class="text-center">
-          <h3 class="text-h2 q-my-sm">Welcome</h3>
-        </header>
-        <article class="container q-mx-auto">
-          <div class="row justify-center">
-            <div class="col-12 col-mx-auto text-center">
-              <div class="container">
-                <p class="text-h4">To begin please add this page to your device's home screen.</p>
-                <q-separator class="q-my-md"/>
-                <div class="row items-center">
-                  <div class="col-6">
-                    <div class="container">
-                      <div class="row q-gutter-md">
-                        <div class="col-2">
-                          <figure class="text-right q-ma-none">
-                            <img src="statics/ios-share-icon.png" style="height:64px" />
-                          </figure>
-                        </div>
-                        <div class="col-8">
-                          <div class="container text-left">
-                            <h6 class="text-h5 q-my-xs">For iOS</h6>
-                            <p class="text-body1">Tap the share icon on your browser and select the <strong>"Add to Home Screen"</strong> icon.</p>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="col-6">
-                    <div class="container">
-                      <div class="row q-gutter-md">
-                        <div class="col-2">
-                          <figure class="text-right q-ma-none">
-                            <img src="statics/options-icon.png" class="q-mt-md" style="height: 46px"/>
-                          </figure>
-                        </div>
-                        <div class="col-8">
-                          <div class="container text-left">
-                            <h6 class="text-h5 q-my-xs">For Android</h6>
-                            <p class="text-body1">If you did not recieve a notification. Tap the options icon on your browser and select the <strong>"Add to Home Screen"</strong> option</p>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="row q-my-md">
-                  <div class="col-12">
-                    <div class="container">
-                      <p class="text-body1">Once this is done, close this browser and open the app using the icon on the Home Screen.</p>
-                    </div>
-                  </div>
-                </div>
+        <article class="container width-75 fixed-center q-mx-auto">
+          <h5 class="text-h4 q-my-sm text-center">Add to Home Screen</h5>
+          <div class="row q-gutter-md" v-if="device == 'ios'"><div class="col-2"><figure class="text-right q-ma-none"><img src="statics/ios-share-icon.png" style="height: 64px;"></figure></div><div class="col-8"><div class="container text-left"><h6 class="text-h5 q-my-xs">For iOS</h6><p class="text-body1">Tap the share icon on your browser and select the <strong>"Add to Home Screen"</strong> icon.</p></div></div></div>
+          <div class="row q-gutter-md" v-if="device ='android'">
+            <div class="col-2">
+              <figure class="text-right q-ma-none">
+                <img src="statics/options-icon.png" class="q-mt-md" style="height: 46px"/>
+              </figure>
+            </div>
+            <div class="col-8">
+              <div class="container text-left">
+                <h6 class="text-h5 q-my-xs">For Android</h6>
+                <p class="text-body1">If you did not recieve a notification. Tap the options icon on your browser and select the <strong>"Add to Home Screen"</strong> option</p>
               </div>
             </div>
           </div>
@@ -73,10 +34,13 @@
     data() {
       return {
         standalone: false,
+        device: null
       }
     },
     created() {
-      this.standalone = isRunningStandalone()
+      let agent = navigator.userAgent || navigator.vendor || window.opera
+      this.standalone = false ///!isRunningStandalone()
+      this.device = (/android/i).test(agent) ?  'android' : ((/ipad/).test(agent)? 'ios' : null )
     }
   }
 </script>
